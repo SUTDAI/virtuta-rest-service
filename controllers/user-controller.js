fc66d4551e961268d4b13eeebe4ec8dcfde1f3f2
lohlifer <lohjaden25@gmail.com>
2024-01-30 13:11:54 +0800

    delete user function implemented

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,23 @@
 const models = require("../models");
 
-function index(request, response) {
-  response.send("Usernames");
+function login(request, response) {
+  const login_data = {
+    userName: request.body.userName,
+    password: request.body.password,
+  };
+
+  //check for valid fields
+  if (!request.body.userName) {
+    response.status(400).send({
+      message: "Username cannot be empty!",
+    });
+  } else if (!request.body.password) {
+    response.status(400).send({
+      message: "Password cannot be empty!",
+    });
+  }
+
+  //unimplemented
 }
 
 function add_user(request, response) {
@@ -10,7 +26,7 @@ function add_user(request, response) {
     password: request.body.password,
     email: request.body.email,
   };
-  
+
   //check for valid fields
   if (!request.body.userName) {
     response.status(400).send({
@@ -45,7 +61,51 @@ function add_user(request, response) {
     });
 }
 
+function remove_user(request, response) {
+  const validation_data = {
+    userName: request.body.userName,
+    password: request.body.password,
+    email: request.body.email,
+  };
+
+  //check for valid fields
+  if (!request.body.userName) {
+    response.status(400).send({
+      message: "Username cannot be empty!",
+    });
+    return;
+  } else if (!request.body.password) {
+    response.status(400).send({
+      message: "Password cannot be empty!",
+    });
+    return;
+  } else if (!request.body.email) {
+    response.status(400).send({
+      message: "Email cannot be empty!",
+    });
+    return;
+  }
+
+  //Catch uploading error
+  models.User.destroy({
+    where: validation_data,
+  })
+    .then((result) => {
+      response.status(201).json({
+        message: "User Deleted Successfully!",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      response.status(500).json({
+        message: "Something Went Wrong",
+        post: error,
+      });
+    });
+}
+
 module.exports = {
-  index: index,
+  login: login,
   add_user: add_user,
+  remove_user: remove_user,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const userController = require("../controllers/user-controller");
 
 const router = express.Router();
 
+router.get("/login", userController.login);
 router.get("/add", userController.add_user);
+router.get("/remove", userController.remove_user);
 
 module.exports = router;
